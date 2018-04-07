6f19c0218aba0de55532e634743d4c297dacc97d
edwardkerry <edwardkerry@gmail.com>
2018-04-07 20:45:41 +0100

    Refactor dashboard tests

--- src/app/components/Dashboard/dashboard.spec.js
+++ src/app/components/Dashboard/dashboard.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 
 import { creditScoreReport, longTermDebtReport } from '../../../../tests/mocks/fixtures'
 import mockAPIResponse from '../../../../tests/mocks/mockAPIResponse'
@@ -11,43 +10,36 @@ import ReportContainer from '../ReportContainer/ReportContainer.jsx'
 describe('Dashboard', () => {
   describe('when loading', () => {
     it('displays a loading sign', () => {
-      const component = renderer.create(<Dashboard />);
-      const json = component.toJSON();
-      expect(json).toMatchSnapshot();
+      const component = shallow(<Dashboard />);
+      const loadingSign = component.find(Loading);
+      expect(loadingSign.length).toEqual(1);
     });
   });
 
   describe('when loaded', () => {
-    it('displays a report container', () => {
-      const component = renderer.create(<Dashboard loading={false} />);
-      const json = component.toJSON();
-      expect(json).toMatchSnapshot();
-    });
-
+    const component = shallow(<Dashboard />);
     
+    component.setState({
+      loading: false,
+      creditScore: creditScoreReport,
+      longTermDebt: longTermDebtReport,
+    });
+    
+    const reportContainer = component.find(ReportContainer);
+    
+    it('does not display a loading sign', () => {
+      const loadingSign = component.find(Loading);
+      expect(loadingSign.length).toEqual(0);
+    });
+
+    it('displays a report container', () => {
+      expect(reportContainer.length).toEqual(1);
+    });
+
     it('passes state to children as props', () => {
-      const component = mount(<Dashboard />);
-  
-      component.setState({
-        loading: false,
-        creditScore: creditScoreReport,
-        longTermDebt: longTermDebtReport,
-      });
-  
-      const result = component.find(ReportContainer);
-      expect(result.prop('creditScore')).toEqual(creditScoreReport);
-      expect(result.prop('longTermDebt')).toEqual(longTermDebtReport);
+      expect(reportContainer.prop('creditScore')).toEqual(creditScoreReport);
+      expect(reportContainer.prop('longTermDebt')).toEqual(longTermDebtReport);
     });
   });
-
-  xdescribe('componentDidMount', () => {
-
-    it('sets loading to false after fetching data', (done) => {
-      const component = shallow(<Dashboard />);
-      component.instance().componentDidMount();
-      expect(component.state('loading')).toEqual(false);
-    });
-  });
-
 
 });
--- tests/config/jest/enzymeSetup.js
+++ tests/config/jest/enzymeSetup.js
@@ -1,10 +1,9 @@
-import { configure, shallow, mount } from 'enzyme'
+import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
 global.shallow = shallow;
-global.mount = mount;
 
 window.matchMedia = window.matchMedia || (() => { 
   return { 
